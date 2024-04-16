b74925745b43058a411c5e2ad4e3b75dd4242a5c
lucasmarti11 <166542097+lucasmarti11@users.noreply.github.com>
2024-04-16 20:41:36 +0200

    Update Ventilador.h

--- Ventilador.h
+++ Ventilador.h
@@ -1,1 +1,38 @@
 
+#include <Arduino.h>
+#include <stdint.h>
+
+#ifndef Ventilador_H
+#define Ventilador_H
+
+typedef enum {ThreePins, FourPins_Soft, FourPins_Hard} FanType_t;
+typedef enum {MANUAL, AUTOMATIC} Operation_Mode_t;
+typedef enum {NORMAL, ECO, GAMING} Function_Mode_t;
+
+
+class Ventilador {
+  public:
+    // Initializations
+
+    Ventilador(FanType_t TipoVent, uint8_t pin_rpm, uint8_t pin_fan, uint8_t pin_pwm = 0);
+
+    uint16_t medirRPM();
+
+    
+    void SetFan(Operation_Mode_t Oper_Mode, Function_Mode_t Func_Mode, uint8_t Temperature, uint8_t last_Temperature, uint8_t percent = 0);
+    
+    
+
+  private:
+    FanType_t FanType;
+    uint8_t pin_revoluciones;
+    uint8_t pin_pcFan;
+    uint8_t pin_pcPWM;
+
+    uint16_t tiempo_inicial;
+    uint16_t tiempo_final;
+    uint16_t tiempo_total;
+    bool counting;
+};
+
+#endif
