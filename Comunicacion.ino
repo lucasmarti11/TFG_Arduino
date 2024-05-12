52ee9f3b6b87bcd4ad45e6ab8ce94a81b6ce5992
lucasmarti11 <166542097+lucasmarti11@users.noreply.github.com>
2024-05-12 21:47:01 +0200

    Update Comunicacion.ino

--- Comunicacion.ino
+++ Comunicacion.ino
@@ -7,15 +7,14 @@ Operation_Mode_t mode_fan = NORMAL;
 float temp_sensor1 = 85.0;
 float temp_sensor2 = 80.0;
 float temp_sensor3 = 60.0;
-uint16_t rpm_fan1 = 1500;
-uint16_t rpm_fan2 = 1300;
-uint16_t rpm_fan3 = 1200;
-uint16_t rpm_fan4 = 1100;
-uint16_t rpm_fan5 = 1800;
+volatile uint16_t rpm_fan1 = 1500;
+volatile uint16_t rpm_fan2 = 1300;
+volatile uint16_t rpm_fan3 = 1200;
+volatile uint16_t rpm_fan4 = 1100;
+volatile uint16_t rpm_fan5 = 1800;
 
 char data[200];
 int data_index = 0;
-bool found_c = false;
 
 void setup() {
   // put your setup code here, to run once:
@@ -77,19 +76,47 @@ void loop() {
     temp_sensor3 -= 70.0;
   }*/
 
-  while(Serial.available() > 0){
+  /*while(Serial.available() > 0){
+    rpm_fan1 = 2000;
     char character = Serial.read();
     if (character != '\n')
     {
       data[data_index] = character;
       data_index ++;
-    }else if(!Serial.available()){
+      rpm_fan4++;
+    }else{
       Serial.print("El mensaje recibido es: ");
       Serial.println(data);
+      //delay(5000);
+      rpm_fan2 = 2000;
       receiveData(data, data_index);
       data_index = 0;
     }
+  }*/
+
+  // Check if there are any characters available to read
+  if(Serial.available() > 0) {
+    // Read the incoming byte
+    char character = Serial.read();
+    // Check if the character is the end of the message ('\n')
+    if(character == '\n'){
+      // Process the received data
+      Serial.print("El mensaje recibido es: ");
+      Serial.println(data);
+      receiveData(data, data_index);
+      // Reset the data index for the next message
+      data_index = 0;
+    }else{
+      // Store the received character in the data buffer
+      data[data_index] = character;
+      data_index++;
+      // Check if the data index exceeds the buffer size to avoid overflow
+      if(data_index >= sizeof(data)){
+        data_index = sizeof(data) - 1; // Ensure data_index stays within bounds
+      }
+    }
   }
+
 }
 
 void sendData(){
@@ -112,26 +139,32 @@ void sendData(){
 void receiveData(char* data1, int index){
 
    StaticJsonDocument<300> doc2;
-
-  //char json2[] = "{\"mode\":2,\"rpm1\":1400,\"rpm2\":1300,\"rpm3\":1400,\"rpm4\":1600,\"rpm5\":1500}"; Formato que no funciona
   //char json3[] = {"mode":2,"rpm1":1400,"rpm2":1300,"rpm3":1400,"rpm4":1600,"rpm5":1500}; Formato que funciona para recibir
-  //Format que s'esta enviant a la primera proba: [{"mode":10},{"pc_rpm1":90},{"pc_rpm2":50},{"pc_rpm3":30},{"pc_rpm4":45},{"pc_rpm5":54}]
   char json[index + 1];
 
   //Amb el seguen bucle el que es vol aconseguir es llevar les comilles de l'inici i del final del string json que genera node red
   for(int i = 0; i < index; i++){
-    if(data1[i] == '}'){
-      json[i] = data1[i];
-      break;
-    }
-    
-    if(data1[i] == '"' && found_c == false){
-      found_c = true;
+    /*if(data1[i+1] != '}'){
+      json[i] = data1[i+1]; // corregir aixo
     }else{
-      json[i] = data1[i];
-    }
+      json[i] = data1[i+1];
+      i++;
+      json[i] = '\n';
+    }*/
+    
+    json[i] = data1[i];
+
+    /*Serial.print("Indice total: ");
+    Serial.println(index);
+    Serial.print("Indice: ");
+    Serial.print(i);
+    Serial.print(" tiene el caracter: ");
+    Serial.println(data1[i]);
+    delay(200);*/
+    
     
   }
+  
   for (int j = 0; j < index; j++) {
     if (json[j] == '\0') {
       // Si encontramos un carácter nulo, lo establecemos y salimos del ciclo
@@ -139,16 +172,19 @@ void receiveData(char* data1, int index){
       break;
     }
   }
-
+  rpm_fan3= 2000;
   /*Serial.print("El mensaje JSON es    : ");
   Serial.println(json2);*/
 
+  /*Serial.print("El mensaje JSON recibido es: ");
+  Serial.println(json);
+  delay(5000);*/
 
   DeserializationError error = deserializeJson(doc2, json);
 
   if (error) {
-    Serial.print(F("deserializeJson() failed: "));
-    Serial.println(error.f_str());
+    /*Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());*/
     return;
   }
 
@@ -158,6 +194,8 @@ void receiveData(char* data1, int index){
   rpm_fan3 = doc2["pc_rpm3"];
   rpm_fan4 = doc2["pc_rpm4"];
   rpm_fan5 = doc2["pc_rpm5"];
+
+
 
   // Print values.
   Serial.println("Received Data: ");
